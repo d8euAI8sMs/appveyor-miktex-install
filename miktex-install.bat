1fb714051574f41044cceb23842017c291d72309
kalaider <a.kalaider@yandex.ru>
2017-10-27 23:18:49 +0300

    Incorrect mpm package installation command fixed

--- miktex-install.bat
+++ miktex-install.bat
@@ -4,8 +4,8 @@ appveyor DownloadFile http://mirrors.ctan.org/systems/win32/miktex/setup/miktex-
 
 7z x miktex-portable.exe -oC:\miktex >NUL
 
-mpm --install --verbose cm-super
-mpm --install --verbose amstex
+mpm --verbose --install cm-super
+mpm --verbose --install amstex
 
 cp -f language.dat c:\miktex\texmfs\install\tex\generic\config\language.dat
 cp -f language.dat.lua c:\miktex\texmfs\install\tex\generic\config\language.dat.lua
