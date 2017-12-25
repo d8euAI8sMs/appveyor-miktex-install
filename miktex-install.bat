59a0c65912756282cf155d25adce43a0d7c1ca77
kalaider <a.kalaider@yandex.ru>
2017-12-25 04:52:15 +0300

    Beamer packages added

--- miktex-install.bat
+++ miktex-install.bat
@@ -7,6 +7,10 @@ appveyor DownloadFile http://mirrors.ctan.org/systems/win32/miktex/setup/miktex-
 mpm --verbose --install cm-super
 mpm --verbose --install amstex
 
+mpm --verbose --install beamer
+mpm --verbose --install pgf
+mpm --verbose --install xcolor
+
 cp -f language.dat c:\miktex\texmfs\install\tex\generic\config\language.dat
 cp -f language.dat.lua c:\miktex\texmfs\install\tex\generic\config\language.dat.lua
 
